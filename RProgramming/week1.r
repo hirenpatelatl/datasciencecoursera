0d0c27757f5e771f55da50f84f23c4ceeb2c95b1
hpq9k4i <hiren_patel@homedepot.com>
2016-03-12 05:17:09 -0500

    added Rprogramming folder and started week 1 excercises

---
+++ RProgramming/week1.r
@@ -0,0 +1,23 @@
+setwd("~/GitHub/datasciencecoursera/RProgramming")
+getwd()
+
+myfunction <- function(x) {
+	y <- rnorm(100)
+	mean(y)
+}
+
+second <- function(x) {
+	x + rnorm(length(x))
+}
+
+.week1.notes = function () {
+
+## week1 notes
+#	R has 5 basic or atomic classes of objects
+#	character
+#	numeric (real numbers)
+#	integer
+#	complex
+#	logical
+
+}
